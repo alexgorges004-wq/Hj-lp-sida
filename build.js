86701d09cd3bba09d97e046734bed5d87e737ebd
alexgorges004-wq <alexgorges004@gmail.com>
2026-09-06 23:04:37 +0200

    Include movable text blocks in static build

--- build.js
+++ build.js
@@ -5,15 +5,18 @@ const out = path.join(__dirname, "dist");
 fs.rmSync(out, {recursive:true, force:true});
 fs.mkdirSync(out, {recursive:true});
 
-for (const file of ["index.html","app.js","config.js","frontpage-admin.js","recovery.js"]) {
+for (const file of ["index.html","app.js","config.js","frontpage-admin.js","home-text-blocks.js","recovery.js"]) {
   fs.copyFileSync(path.join(__dirname,file), path.join(out,file));
 }
 
 const indexPath = path.join(out, "index.html");
 let html = fs.readFileSync(indexPath, "utf8");
+if (!html.includes('src="home-text-blocks.js"')) {
+  html = html.replace("</body>", '<script src="home-text-blocks.js"></script>\n</body>');
+}
 if (!html.includes('src="recovery.js"')) {
   html = html.replace("</body>", '<script src="recovery.js"></script>\n</body>');
-  fs.writeFileSync(indexPath, html);
 }
+fs.writeFileSync(indexPath, html);
 
 console.log("Built static site to dist/");
