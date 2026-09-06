b8b7ad121f1423e901363991d8b34738d17c1824
alexgorges004-wq <alexgorges004@gmail.com>
2026-09-06 14:31:38 +0200

    Add Appwrite configuration template

---
+++ config.js
@@ -0,0 +1,11 @@
+// Appwrite client configuration.
+// These values are NOT secret API keys. They are safe to be used by the browser.
+// Fill them in after creating the Appwrite project.
+
+window.LIVESTREAM_APPWRITE = {
+  endpoint: "YOUR_APPWRITE_ENDPOINT",       // e.g. https://fra.cloud.appwrite.io/v1
+  projectId: "YOUR_PROJECT_ID",
+  databaseId: "YOUR_DATABASE_ID",
+  tableId: "YOUR_TABLE_ID",
+  rowId: "main"
+};
