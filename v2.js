8ab1d818ad2055eb554100a9214cd497bb74d023
alexgorges004-wq <alexgorges004@gmail.com>
2026-09-07 23:06:11 +0200

    Load text block height fix

--- v2.js
+++ v2.js
@@ -1,1 +1,1 @@
-document.write('<script src="v2-core.js"><\/script><script src="v2-layout.js"><\/script><script src="v2-tools.js"><\/script>');
+document.write('<script src="v2-core.js"><\/script><script src="v2-layout.js"><\/script><script src="v2-tools.js"><\/script><script src="v2-height-fix.js"><\/script>');
