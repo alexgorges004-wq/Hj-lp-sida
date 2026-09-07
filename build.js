e25b6c4cfae7a6afdf5231fdf76f5a342ac1bdc5
alexgorges004-wq <alexgorges004@gmail.com>
2026-09-07 22:29:34 +0200

    Build v2 modules

--- build.js
+++ build.js
@@ -2,11 +2,24 @@ const fs = require("fs");
 const path = require("path");
 
 const out = path.join(__dirname, "dist");
-fs.rmSync(out, {recursive:true, force:true});
-fs.mkdirSync(out, {recursive:true});
+fs.rmSync(out, { recursive: true, force: true });
+fs.mkdirSync(out, { recursive: true });
 
-for (const file of ["index.html","app.js","config.js","frontpage-admin.js","frontpage-core.js","home-text-blocks.js","recovery.js","polish.js","favicon.ico"]) {
-  fs.copyFileSync(path.join(__dirname,file), path.join(out,file));
+for (const file of [
+  "index.html",
+  "app.js",
+  "config.js",
+  "frontpage-core.js",
+  "home-text-blocks.js",
+  "recovery.js",
+  "polish.js",
+  "v2.js",
+  "v2-core.js",
+  "v2-layout.js",
+  "v2-tools.js",
+  "favicon.ico"
+]) {
+  fs.copyFileSync(path.join(__dirname, file), path.join(out, file));
 }
 
 console.log("Built static site to dist/");
